dba38f07b9402e686889fdd21cdcf3edf470035b
JernejHren <106534210+JernejHren@users.noreply.github.com>
2025-12-11 17:26:06 +0100

    Create __init__.py

---
+++ custom_components/__init__.py
@@ -0,0 +1,38 @@
+"""EVSCI Integracija - Inicializacija."""
+import logging
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+from .coordinator import EVSCICoordinator
+
+# DODANO: "number" na seznam
+PLATFORMS = ["select", "sensor", "time", "number"]
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Nastavi integracijo."""
+    hass.data.setdefault(DOMAIN, {})
+
+    coordinator = EVSCICoordinator(hass, entry)
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data[DOMAIN][entry.entry_id] = coordinator
+    
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
+    entry.async_on_unload(entry.add_update_listener(update_listener))
+
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Odstrani integracijo."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
+
+async def update_listener(hass: HomeAssistant, entry: ConfigEntry):
+    """Sprememba nastavitev."""
+    await hass.config_entries.async_reload(entry.entry_id)
